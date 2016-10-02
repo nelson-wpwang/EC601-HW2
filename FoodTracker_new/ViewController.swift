b7300b1cb8eaeec8cb9de145c053a7c1f0e7c1f3
Nelson Wang <NelsonWang@Nelsons-MacBook-Pro.local>
2016-10-02 17:59:05 -0400

    Initial Commit

---
+++ FoodTracker_new/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker_new
+//
+//  Created by Nelson Wang on 10/2/16.
+//  Copyright © 2016 BU ECE WENPENG WANG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
