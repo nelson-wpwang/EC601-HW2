e2efab4381d15dc4ff3108d92a4cabd57bdf5142
Nelson Wang <NelsonWang@Nelsons-MacBook-Pro.local>
2016-10-02 22:48:00 -0400

    first commit

---
+++ FoodTracker_new/MealTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker_new
+//
+//  Created by Nelson Wang on 10/2/16.
+//  Copyright © 2016 BU ECE WENPENG WANG. All rights reserved.
+//
+
+import Foundation
---
+++ RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker_new
+//
+//  Created by Nelson Wang on 10/2/16.
+//  Copyright © 2016 BU ECE WENPENG WANG. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
